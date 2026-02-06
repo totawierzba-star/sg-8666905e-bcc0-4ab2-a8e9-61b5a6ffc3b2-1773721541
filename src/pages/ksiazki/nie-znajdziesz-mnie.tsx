10b5466500f909a33011f472a2a28c71179932ee
Softgen Dev <dev@softgen.ai>
2026-02-06 13:41:33 +0000

    Updates

--- src/pages/ksiazki/nie-znajdziesz-mnie.tsx
+++ src/pages/ksiazki/nie-znajdziesz-mnie.tsx
@@ -367,7 +367,7 @@ export default function NieZnajdzieszMniePage() {
               <Button asChild variant="outline" size="lg">
                 <Link href="/artykuly/jak-rozmawiac-o-niepelnosprawnosci">
                   <BookOpen className="mr-2 h-5 w-5" />
-                  Dowiedz się więcej o biblioterapii
+                  Dowiedz się więcej o rozmowach o niepełnosprawności
                 </Link>
               </Button>
             </div>
